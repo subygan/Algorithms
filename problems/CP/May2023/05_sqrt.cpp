5ba982159e6be69504da855f1ba928c07e5ec157
suriya ganesh <suriya@suriyas-MacBook-Pro.local>
2023-05-05 20:16:31 +0530

    May 05 problem

---
+++ problems/CP/May2023/05_sqrt.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int sqrt(int x){
+
+    long long int mid;
+    int low = 0;
+    int high = x;
+    if (x < 2) return x;
+    while (low < high) {
+        mid = (low + high) / 2;
+        long long int sqr = mid * mid;
+        if (sqr == x) return mid;
+        if (sqr >  x) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    if (mid*mid > x) return mid - 1;
+
+    return mid;
+}
+
+int main(){
+    cout<<sqrt(0);
+}
